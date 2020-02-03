a5105d81aa1e35d6d0ea7dd420f95621209e9108
keshav.gaire <keshav.gaire@itc-engineering.com>
2020-02-03 15:51:03 +0100

    adding bootstrap and updating UI

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
 import { AppRouteModule } from './app-route.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -14,6 +15,9 @@ import { SalesComponent} from './sales/sales.component';
 import { SalesService} from './sales/sales.service';
 import { HomeComponent } from './home/home.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import {root} from 'rxjs/internal-compatibility';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +27,9 @@ import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
     ItemListComponent,
     HomeComponent,
     PagenotfoundComponent,
-    SalesComponent
+    SalesComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
